3990de012066abac87745f352a00200a60e163d0
Can Kanbur <cankanbur1@gmail.com>
2022-11-23 23:52:00 +0300

    added coin detail screen

--- src/screens/CoinDetailsScreen/index.js
+++ src/screens/CoinDetailsScreen/index.js
@@ -1,10 +1,19 @@
-import { View, Text, Image } from 'react-native';
-import React from 'react';
+import { View, Text, Dimensions, TextInput } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  ChartDot,
+  ChartPath,
+  ChartPathProvider,
+  ChartYLabel,
+} from '@rainbow-me/animated-charts';
+import { AntDesign } from '@expo/vector-icons';
+
 import coin from '../../../data/crypto.json';
 import styles from './styles';
 import CoinDetailHeader from './components/CoinDetailHeader';
-import PriceInfo from './components/PriceInfo';
+
 const CoinDetailScreen = () => {
+  const { width } = Dimensions.get('window');
   const {
     image: { small },
     name,
@@ -14,20 +23,113 @@ const CoinDetailScreen = () => {
       price_change_percentage_24h,
     },
     symbol,
+    prices,
   } = coin;
+  const [coinValue, setCoinValue] = useState('1');
+  const [usdValue, setUsdValue] = useState(current_price.usd.toString());
+  const changeCoinValue = value => {
+    setCoinValue(value);
+    const floatedValue = parseFloat(value.replace(',', '.')) || 0;
+    setUsdValue((floatedValue * current_price.usd).toString());
+  };
+  const changeUsdValue = value => {
+    setUsdValue(value);
+    const floatedValue = parseFloat(value.replace(',', '.')) || 0;
+    setCoinValue((floatedValue / current_price.usd).toString());
+  };
+  useEffect(() => {}, [coinValue]);
+  useEffect(() => {}, [usdValue]);
 
+  const percentageColor =
+    price_change_percentage_24h < 0 ? '#fc5c65' : '#4cd964';
+  const formatCurrency = value => {
+    'worklet';
+    if (value == '') {
+      return `$${current_price.usd.toFixed(2)}`;
+    }
+    return `$${parseFloat(value.toFixed(2))}`;
+  };
+  const chatColor = current_price.usd > prices[0][1] ? '#4cd964' : '#fc5c65';
   return (
     <View style={styles.container}>
-      <CoinDetailHeader
-        small={small}
-        symbol={symbol}
-        market_cap_rank={market_cap_rank}
-      />
-      <PriceInfo
-        name={name}
-        current_price={current_price}
-        price_change_percentage_24h={price_change_percentage_24h}
-      />
+      <ChartPathProvider
+        data={{
+          points: prices.map(prices => ({
+            x: prices[0],
+            y: prices[1],
+          })),
+          smoothingStrategy: 'bezier',
+        }}
+      >
+        <CoinDetailHeader
+          small={small}
+          symbol={symbol}
+          market_cap_rank={market_cap_rank}
+        />
+        <View style={styles.priceContainer}>
+          <View>
+            <Text style={styles.name}>{name}</Text>
+            <ChartYLabel format={formatCurrency} style={styles.currentPrice} />
+          </View>
+          <View
+            style={{
+              borderRadius: 5,
+              paddingHorizontal: 5,
+              flexDirection: 'row',
+              backgroundColor: percentageColor,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <AntDesign
+              name={price_change_percentage_24h < 0 ? 'caretdown' : 'caretup'}
+              size={12}
+              style={{
+                alignSelf: 'center',
+                marginHorizontal: 10,
+              }}
+              color="white"
+            />
+            <Text style={styles.priceChange}>
+              {price_change_percentage_24h.toFixed(2)} %
+            </Text>
+          </View>
+        </View>
+        <View>
+          <ChartDot style={{ backgroundColor: chatColor }} />
+
+          <ChartPath
+            height={width / 2}
+            stroke={chatColor}
+            width={width}
+            strokeWidth={2}
+          />
+        </View>
+        <View style={{ flexDirection: 'row' }}>
+          <View style={{ flexDirection: 'row', flex: 1 }}>
+            <Text style={{ color: 'white', alignSelf: 'center' }}>
+              {symbol.toUpperCase()}
+            </Text>
+            <TextInput
+              style={styles.input}
+              value={coinValue}
+              keyboardType="numeric"
+              onChangeText={changeCoinValue}
+              changeUsdValue
+            />
+          </View>
+
+          <View style={{ flexDirection: 'row', flex: 1 }}>
+            <Text style={{ color: 'white', alignSelf: 'center' }}>USD</Text>
+            <TextInput
+              style={styles.input}
+              keyboardType="numeric"
+              value={usdValue}
+              onChangeText={changeUsdValue}
+            />
+          </View>
+        </View>
+      </ChartPathProvider>
     </View>
   );
 };
--- src/screens/CoinDetailsScreen/styles.js
+++ src/screens/CoinDetailsScreen/styles.js
@@ -5,5 +5,35 @@ export default StyleSheet.create({
     flex: 1,
     padding: 10,
   },
-  
+  name: {
+    color: 'white',
+    fontSize: 15,
+  },
+  priceContainer: {
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  currentPrice: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: '600',
+    letterSpacing: 1,
+  },
+  priceChange: {
+    color: 'white',
+    fontSize: 17,
+    fontWeight: '600',
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    margin: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: 'white',
+    padding: 10,
+    fontSize: 18,
+    color: 'white',
+  },
 });
