e27ab8171530f9a2f2aa3b00867b9b094197c3fa
Can Kanbur <cankanbur1@gmail.com>
2022-11-23 16:53:04 +0300

    added components

--- src/screens/CoinDetailsScreen/index.js
+++ src/screens/CoinDetailsScreen/index.js
@@ -3,19 +3,30 @@ import React from 'react';
 import coin from '../../../data/crypto.json';
 import styles from './styles';
 import CoinDetailHeader from './components/CoinDetailHeader';
+import PriceInfo from './components/PriceInfo';
 const CoinDetailScreen = () => {
   const {
     image: { small },
     name,
-    market_data: { market_cap_rank },
+    market_data: {
+      market_cap_rank,
+      current_price,
+      price_change_percentage_24h,
+    },
     symbol,
   } = coin;
+
   return (
     <View style={styles.container}>
       <CoinDetailHeader
         small={small}
         symbol={symbol}
         market_cap_rank={market_cap_rank}
+      />
+      <PriceInfo
+        name={name}
+        current_price={current_price}
+        price_change_percentage_24h={price_change_percentage_24h}
       />
     </View>
   );
